5890ae0617a2f4f4459ac825320023ca35b1c227
Thomas Lane <parkerlane@Thomass-Mac-mini.local>
2021-04-08 11:30:16 -0400

    More Admin Changes

--- controllers/userFormView.js
+++ controllers/userFormView.js
@@ -7,15 +7,19 @@ exports.index = async function (req, res, next) {
   // queries the DB to return information about the selected form that brought them to this page.
   await db
     .any(
-      `select c.company_name, f.form_responses, f3.attribute_name, f4.value 
+      `select f4.attrib_id, c.company_name, f3.attribute_name, f4.value, f4.staff_id 
             from company c 
             inner join formresponse f on c.id = f.company_id 
             inner join forms f2 on f.form_id = f2.form_id 
             inner join formquestion f3 on f2.form_id = f3.form_id 
             inner join formquestionresponse f4 on f3.attrib_id  = f4.attrib_id 
             where f.company_id = $1 AND f.form_id = $2 AND f.response_id = $3 AND f4.response_id = $3
-            order by f3.attribute_name`,
-      [req.params.companyid, req.params.formid, req.params.formresponseid]
+            order by min(f4.attrib_id) over (partition by f4.staff_id), f4.staff_id, f4.attrib_id`,
+      [
+        parseInt(req.params.companyid),
+        parseInt(req.params.formid),
+        parseInt(req.params.formresponseid),
+      ]
     )
     .then((results) => {
       // create a doc
