f4dd9d3f851be2d8a23a308bf552efecac388268
Thomas Lane <parkerlane@Thomass-Mac-mini.local>
2021-04-08 14:59:07 -0400

    Fixed Listing

--- controllers/userFormView.js
+++ controllers/userFormView.js
@@ -14,7 +14,10 @@ exports.index = async function (req, res, next) {
       inner join formquestion f3 on f2.form_id = f3.form_id 
       inner join formquestionresponse f4 on f3.attrib_id  = f4.attrib_id 
       where f.company_id = $1 AND f.form_id = $2 AND f.response_id = $3 AND f4.response_id = $3
-      order by min(f4.attrib_id) over (partition by f4.staff_id), f4.staff_id , f4.attrib_id
+      order by case when f4.staff_id is null then f4.attrib_id else min(f4.attrib_id) over (partition by f4.staff_id) end,
+   f4.staff_id,
+    f4.attrib_id;
+
 `,
       [
         parseInt(req.params.companyid),
