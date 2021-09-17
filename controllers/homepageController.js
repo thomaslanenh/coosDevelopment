f61dbac046a6c5d2420ab97fd8ec1d218bbf89dc
Thomas Lane <tlane@Thomass-MBP.lan>
2021-09-17 11:13:39 -0400

    More updated Names

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,5 +1,5 @@
 var currentYear = new Date().getFullYear();
 
 exports.index = function (req, res, next) {
-  res.render("index", {title: "Mothership", user: req.user, currentYear})
+  res.render("index", {title: "Coös County Director Network (CCDN) Training Quality Database", user: req.user, currentYear})
 };
